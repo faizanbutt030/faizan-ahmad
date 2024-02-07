b418fdcea56f1a6296f539645ccb8340c8691200
Faizan Ahmad <faizanbutt030@gmail.com>
2024-02-07 17:05:28 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import Footer from "./components/Footer/footer";
 function App() {
   return (
     <div className="App">
+          <head><script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8124571476097533"
+     crossorigin="anonymous"></script></head>
       <Navbar />
       <Intro />
       <Skills /> 
